1615891b3226f74a6d503d1f6d2d34e5cb886d77
himansu <himansu.behera89@gmail.com>
2021-05-20 08:26:54 +0530

    revert burger

--- hungry.py
+++ hungry.py
@@ -2,6 +2,10 @@ hungry=input('are you hungry ?')
 if hungry=='yes':
     print('eat somas')
     print('eat pizza')
+<<<<<<< HEAD
     print('this')
+=======
+    print('eat burger')
+>>>>>>> parent of 021bb18 (Revert "added burger")
 else:
     print('go for home work')
