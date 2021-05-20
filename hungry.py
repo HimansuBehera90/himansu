4e7280936e2ba2b68e4170476a24e610b02de55b
himansu <himansu.behera89@gmail.com>
2021-05-20 09:29:42 +0530

    added drink option

--- hungry.py
+++ hungry.py
@@ -3,6 +3,10 @@ if hungry=='yes':
     print('eat somas')
     print('eat pizza')
     print('eat burger')
+<<<<<<< thirsty
+=======
+    print('eat fries')
+>>>>>>> local
 else:
     thirsty=input('are you thirsty ?')
     if thirsty=="yes":
